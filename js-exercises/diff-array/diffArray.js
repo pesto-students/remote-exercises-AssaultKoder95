f94af660add1184dd29b43b4bde65cbbbf146106
Abhishek Khanna <abhishekkhanna1995@gmail.com>
2020-05-26 15:59:09 +0530

    add solution for diff array problem

--- js-exercises/diff-array/diffArray.js
+++ js-exercises/diff-array/diffArray.js
@@ -1,3 +1,12 @@
-function diffArray() {}
+function filterUniquePrimaryArrayElements(primaryArray, secondaryArray) {
+  return primaryArray.filter((val) => secondaryArray.indexOf(val) === -1);
+}
+
+function diffArray(inputArray1, inputArray2) {
+  return [
+    ...filterUniquePrimaryArrayElements(inputArray1, inputArray2),
+    ...filterUniquePrimaryArrayElements(inputArray2, inputArray1),
+  ];
+}
 
 export { diffArray };
