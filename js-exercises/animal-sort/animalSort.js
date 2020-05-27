23337eab1e786bdbec4758ceed619313e509af6a
Abhishek Khanna <abhishekkhanna1995@gmail.com>
2020-05-27 11:45:32 +0530

    fix: Add missing solution file

--- js-exercises/animal-sort/animalSort.js
+++ js-exercises/animal-sort/animalSort.js
@@ -1,3 +1,21 @@
-// const animalSort = animals => {};
+function compareAnimalNames(firstAnimalName, secondAnimalName) {
+  if (firstAnimalName < secondAnimalName) {
+    return -1;
+  }
+  if (firstAnimalName > secondAnimalName) {
+    return 1;
+  }
+  return 0;
+}
 
-// export { animalSort };
+const animalSort = (animals) => {
+  return animals.sort((firstAnimal, secondAnimal) => {
+    if (firstAnimal.numberOfLegs === secondAnimal.numberOfLegs) {
+      return compareAnimalNames(firstAnimal.name, secondAnimal.name);
+    }
+
+    return firstAnimal.numberOfLegs - secondAnimal.numberOfLegs;
+  });
+};
+
+export { animalSort };
